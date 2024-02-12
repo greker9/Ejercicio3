dc5b9249a04f74200314a77fcb5d7482c0456003
Greker9 <davidarturo9@gmail.com>
2024-02-11 23:30:04 -0600

    subir contenido al github

--- js/index.js
+++ js/index.js
@@ -1,44 +1,128 @@
-function promedio() {
+
+//formula 1 final
+function metros() {
     let n1 = parseInt(document.getElementById('txtN1').value)
-    let n2 = parseInt(document.getElementById('txtN2').value)
-    let n3 = parseInt(document.getElementById('txtN3').value)
+   
+
 
     if (isNaN(n1)) {
-        alert('Por favor, ingrese todas las notas');
+        alert('Por favor, no deje campos en blanco');
     
-    } else if(isNaN(n2)) {
-        alert('Por favor, ingrese todas las notas');
-       
-       
-    } else if(isNaN(n3)) {
-        alert('Por favor, ingrese todas las notas');
+              
     } else {
-        let promedio = (n1+n2+n3)/3;
-        document.getElementById('txtResultado').value = promedio
+        let metros = (n1*1000);
+        document.getElementById('txtResultado').value = metros
 
-        if (promedio >= 0 && promedio <= 59) {
-            alert("Reprobado");
-        } else if (promedio >= 60 && promedio <= 79) {
-           alert("Bueno");
-        } else if (promedio >= 80 && promedio <= 89) {
-            alert("Muy bueno");
-        } else if (promedio >= 90 && promedio <= 100) {
-           alert("Sobresaliente");
-        } else {
-            alert("El promedio está fuera del rango válido");
-        }
+       
+    }
+}
+
+    function limpiar() {
+        document.getElementById('txtN1').value = ''
+       
+        document.getElementById('txtResultado').value = ''
+    }
 
 
+
+    //formula 1 final
+
+//formula 2 incio
+
+
+
+
+function centimetros() {
+    let n2 = parseInt(document.getElementById('txtN2').value)
+   
+
+
+    if (isNaN(n2)) {
+        alert('Por favor, no deje campos en blanco');
+    
+              
+    } else {
+        let centimetros = (n2*100);
+        document.getElementById('txtResultado2').value = centimetros
+
+       
     }
 
 
 }
 
 
-
-function limpiar() {
-    document.getElementById('txtN1').value = ''
+function limpiar2() {
     document.getElementById('txtN2').value = ''
-    document.getElementById('txtN3').value = ''
-    document.getElementById('txtResultado').value = ''
+   
+    document.getElementById('txtResultado2').value = ''
 }
+//formula 2 final
+
+
+
+//formula 3 incio
+
+
+
+
+function pulgadas() {
+    let n3 = parseInt(document.getElementById('txtN3').value)
+   
+
+
+    if (isNaN(n3)) {
+        alert('Por favor, no deje campos en blanco');
+    
+              
+    } else {
+        let pulgadas = (n3*12);
+        document.getElementById('txtResultado3').value = pulgadas
+
+       
+    }
+
+
+}
+
+
+function limpiar3() {
+    document.getElementById('txtN3').value = ''
+   
+    document.getElementById('txtResultado3').value = ''
+}
+//formula 3 final
+
+
+
+//formula 3 incio
+
+
+
+
+function yardas() {
+    let n4 = parseInt(document.getElementById('txtN4').value)
+   
+
+
+    if (isNaN(n4)) {
+        alert('Por favor, no deje campos en blanco');
+    
+              
+    } else {
+        let yardas = (n4*12);
+        document.getElementById('txtResultado4').value = yardas
+
+       
+    }
+
+
+}
+
+
+function limpiar4() {
+    document.getElementById('txtN4').value = ''
+   
+    document.getElementById('txtResultado4').value = ''
+}
+//formula 3 final
